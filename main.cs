133f931ddc1262e46194906b36829f140e8325a3
Yasmin Tomassoni <yasmintomassoni@gmail.com>
2024-03-01 16:53:44 -0300

    Idades
    Agora o código compara duas idades, sendo a da Yasmin fixa.

--- main.cs
+++ main.cs
@@ -2,9 +2,20 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-
+    
     string Nome = "YASMIN";
-    Console.WriteLine ($"{Nome}!");
+    int IdadeYasmin = 18;
+
+    Console.WriteLine ("Digite sua idade para calcular a diferença de idades");
+    int idadeColeguinha =  int.Parse(Console.ReadLine());
+
+    if (idadeColeguinha>IdadeYasmin){ 
+    Console.WriteLine ($"{Nome} tem {idadeColeguinha- IdadeYasmin} anos de diferença a menos de tu");
+    }
+
+    else{
+    Console.WriteLine ($"{Nome} tem {IdadeYasmin - idadeColeguinha} anos de diferença a mais de tu");
+    }
     
   }
 }
